e03805c62edfcc71830b9ac9ce6b3b31a7f80549
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-05-16 17:01:05 -0700

    added trivia questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,71 @@
 var triviaQuestions = [{
-	question: "In what year was Pixar founded?",
-	answerList: ["1979", "1986", "1995", "2000"],
+	question: "In what city was the rock band BUSH formed?",
+	answerList: ["Seattle", "London", "Los Angeles", "New York"],
 	answer: 1
 },{
-	question: "Which tech mogul provided funding and became a co-founder of Pixar?",
-	answerList: ["Steve Jobs", "Bill Gates", "Peter Thiel", "Mark Zuckerberg"],
+	question: "In what city did the grunge band NIRVANA record thier album \"Nevermind\"?",
+	answerList: ["Los Angeles", "Seattle", "Aberdeen", "Tacoma"],
 	answer: 0
 },{
-	question: "What was Pixar's first feature-length film that was released in 1995?",
-	answerList: ["Toy Story", "A Bug's Life", "Monster's Inc", "Finding Nemo"],
+	question: "Who played drums for the band SOUNDGARDEN in 1984?",
+	answerList: ["Matt Cameron", "Ben Shepherd", "Kim Thayil", "Hiro Yamamoto"],
 	answer: 0
 },{
-	question: "Who was the first Pixar character added to the Disney Princess line-up?",
-	answerList: ["Jessie", "Repunzel", "Merida", "Elsa"],
+	question: "The Long Beach ska band SUBLIME formed in what year?",
+	answerList: ["1990", "1986", "1988", "1993"],
 	answer: 2
 },{
-	question: "What's the name of Pixar's first short film, also known as their mascot?",
-	answerList: ["Lampo", "Junior", "Pixie", "Luxo Jr."],
+	question: "In what year did the band GREEN DAY win the Grammy for thier album \"Dookie\"?",
+	answerList: ["1992", "1998", "1990", "1994"],
 	answer: 3
 },{
-	question: "How many sequels does Pixar currently have released? (as of August 2016)",
-	answerList: ["5", "3", "6", "7"],
+	question: "How many albums has the English rock band OASIS released?",
+	answerList: ["13", "29", "10", "6"],
 	answer: 0
 },{
-	question: "Which film won Pixar's first Academy Award for Best Animated Feature?",
-	answerList: ["Toy Story", "Finding Nemo", "Up", "Wall-E"],
+	question: "What city is the lead singer of PEARL JAM from?",
+	answerList: ["Seattle", "San Diego", "Los Angeles", "Saramento"],
 	answer: 1
 },{
-	question: "Who directed Pixar's first three feature films?",
-	answerList: ["Peter Docter", "Brad Bird", "John Lasseter", "Peter Sohn"],
+	question: "Where did Billy Corgan work before forming SMASHING PUMPKINS?",
+	answerList: ["Gas Station", "Record Store", "Night Club", "Car Dealership"],
+	answer: 1
+},{
+	question: "What was the name of the rock band WEEZER's first single",
+	answerList: ["Say It Aint\'t So", "Buddy Holly", "Undone - The Sweater Song", "Hash Pipe"],
 	answer: 2
 },{
-	question: "Who voiced Sadness in 'Inside Out'?",
-	answerList: ["Amy Poehler", "Phyllis Smith", "Mindy Kaling", "Phyllis Vance"],
-	answer: 1
-},{
-	question: "Billy Crystal voices Mike Wazowski in 'Monster, Inc.' but what role did he originally turn down from Pixar?",
-	answerList: ["Hopper", "Woody", "Marlin", "Buzz Lightyear"],
+	question: "The guitarist Dave Navarro from JANES'S ADDICTION was married to what 90's sex symbol?",
+	answerList: ["Pamela Anderson", "Heather Locklear", "Claudia Schiffer", "Carmen Electra"],
 	answer: 3
 },{
-	question: "The voice of WALL-E, Ben Burtt, also voiced what other famous robot?",
-	answerList: ["R2-D2", "Alpha 5", "C-3PO", "Astro Boy"],
-	answer: 0
-},{
-	question: "Brad Bird directed which animated film prior to taking on 'The Incredibles'?",
-	answerList: ["The Brave Little Toaster", "The Iron Giant", "Tarzan", "The Prince of Egypt"],
+	question: "What was the name of the punk band BLINK 182's debut album?",
+	answerList: ["Enema of the State", "Cheshire Cat", "Take Off Your Pants and Jacket", "Dude Ranch"],
 	answer: 1
 },{
-	question: "Pixar was originally a division of which studio?",
-	answerList: ["Dreamworks", "Industrial Light & Magic", "Disney", "Lucasfilm"],
+	question: "Zack de la Rocha is the lead singer for which Los Angeles based band?",
+	answerList: ["AUDIOSLAVE", "RAGE AGAINST THE MACHINE", "THE VERVE", "COLLECTIVE SOUL"],
+	answer: 1
+},{
+	question: "Gwen Stefani of the ska band NO DOUBT wrote the song \"Don't Speak\" for this musician.",
+	answerList: ["Blake Shelton", "Gavin Rossdale", "Brad Nowell", "Tony Kanal"],
 	answer: 3
 },{
-	question: "What is the name of the famour explorer from 'Up' that Carl looked up to as a boy?",
-	answerList: ["Charles F. Muntz", "Chuck M. James", "Charlie Rose", "Carl Carlton"],
+	question: "The rock band EVERCLEAR was formed in 1992 in which state?",
+	answerList: ["Oregon", "California", "Washington", "Arizona"],
 	answer: 0
 },{
-	question: "'A Bug's Life' was loosely based on what other film?",
-	answerList: ["I Live in Fear", "Rashomon", "Seven Samurai", "Magneficent Seven"],
+	question: "Scottish born Shirley Manson is the lead singer of what band?",
+	answerList: ["Hole", "Sonic Youth", "Garbage", "Pixies"],
 	answer: 2
 }];
 
 var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15'];
 var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
 var messages = {
-	correct: "Yes, that's right!",
-	incorrect: "No, that's not it.",
-	endTime: "Out of time!",
+	correct: "NICE, you got it!",
+	incorrect: "NOPE, that's not it.",
+	endTime: "TIME'S UP! Too slow.",
 	finished: "Alright! Let's see how well you did."
 }
 
@@ -98,7 +98,7 @@ function newQuestion(){
 	answered = true;
 	
 	//sets up new questions & answerList
-	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+' out of '+triviaQuestions.length);
 	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
 	for(var i = 0; i < 4; i++){
 		var choices = $('<div>');
@@ -118,7 +118,7 @@ function newQuestion(){
 
 function countdown(){
 	seconds = 15;
-	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + ' seconds</h3>');
 	answered = true;
 	//sets timer to go down
 	time = setInterval(showCountdown, 1000);
@@ -126,7 +126,7 @@ function countdown(){
 
 function showCountdown(){
 	seconds--;
-	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + ' seconds</h3>');
 	if(seconds < 1){
 		clearInterval(time);
 		answered = false;
